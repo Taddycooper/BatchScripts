04156fbb6622a890a01a176becb6b05c5341d51e
taddycooper <Nathaniel.sweany@gmail.com>
2026-02-16 11:25:31 -0500

    Update placeholder.bat

--- placeholder.bat
+++ placeholder.bat
@@ -1,10 +1,9 @@
 @echo off
-color 1
+color 2
 title f7943fg97eofrg496aogvblghdx78ogyop78htbfxthgiy6nkf
 start https://www.youtube.com/@Taddycooper
 echo My youtube
 pause
 :a
 echo subscribe
-pause
 goto :a
